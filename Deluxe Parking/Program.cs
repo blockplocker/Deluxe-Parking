9d341d3ec9df7e202b8602e6ee3211c04e449d26
blockplocker <noahallklint@gmail.com>
2024-10-29 16:43:00 +0100

    Add project files.

---
+++ Deluxe Parking/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Deluxe_Parking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
